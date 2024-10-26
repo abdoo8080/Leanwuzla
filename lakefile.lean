6a888c631d693b25122cd1e2fae32fce4c75a1f4
Abdalrhman M Mohamed <abdoo8080@outlook.com>
2024-10-26 16:41:32 -0700

    feat: add simple parser for QF_BV fragment of SMT-LIB v2.

--- lakefile.lean
+++ lakefile.lean
@@ -1,9 +1,16 @@
 import Lake
 open Lake DSL
 
+require Qq from git "https://github.com/abdoo8080/quote4.git" @ "master"
+
 package "leanwuzla" where
   version := v!"0.1.0"
 
 @[default_target]
 lean_lib «Leanwuzla» where
   -- add library configuration options here
+
+@[default_target]
+lean_exe «leanwuzla» where
+  root := `Main
+  supportInterpreter := true
